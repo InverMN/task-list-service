985eadba471336006905a1a155c227897a32d984
Inver <jankowskipawelxd@gmail.com>
2021-01-20 11:45:42 +0100

    Upgrade scirpt to change export from paths

--- scripts/append-extensions-to-import-paths.js
+++ scripts/append-extensions-to-import-paths.js
@@ -17,10 +17,11 @@ function appendExtensions(filePath) {
   fs.readFile(filePath, 'utf8', (error, data) => {
     if(error) throw error
 
-    if(!data.match(/import .* from/g)) return
+    if(!data.match(/import .* from/g) && !data.match(/export .* from/g)) return
     let newData = data.replace(/(import .* from\s+['"])(\.\/)(.*)(?=['"])/g, '$1$2$3.js')
+    let finalData = newData.replace(/(export .* from\s+['"])(\.\/)(.*)(?=['"])/g, '$1$2$3.js')
 
-    fs.writeFile(filePath, newData, error => { 
+    fs.writeFile(filePath, finalData, error => { 
       if(error) throw error
     })
   })
